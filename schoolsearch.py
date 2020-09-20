414dea3a571f2f82b87e1d706a50412ec1d5ce85
Thomas Wrappe <twrappe@unix1.csc.calpoly.edu>
2020-09-19 19:42:27 -0700

    Tests and combined schoolsearch

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,4 +1,5 @@
 from sys import *
+from os import path
 
 def teacher(lastname, f_in):
     for line in f_in.readlines():
@@ -64,6 +65,46 @@ def bus(route, f_in):
           if line[4] == route:
              line = line[0] + "," + line[1] + "," + line[2] + "," + line[3]
              print(line)
+def average(gradenumber,f_in):
+    total = 0
+    count = 0
+    studentsGrade = []
+    for student in f_in:
+        student = student.split(",")
+        if student[2] == gradenumber:
+            studentsGrade.append(student[5])
+    for student in studentsGrade:
+        total += float(student)
+        count += 1
+    if count == 0:
+        return 0
+    average = total / count
+    average = round(average,2)
+    print("Grade level: " + str(gradenumber))
+    print("Average GPA: " + str(average))
+    
+
+def info(f_in):
+   classSize = [0]*7
+   for student in f_in.readlines():
+        student = student.split(",")
+        if student[2] == '0':
+           classSize[0] += 1
+        if student[2] == '1':
+           classSize[1] += 1 
+        if student[2] == '2':
+           classSize[2] += 1
+        if student[2] == '3':
+           classSize[3] += 1
+        if student[2] == '4':
+           classSize[4] += 1
+        if student[2] == '5':
+           classSize[5] += 1
+        if student[2] == '6':
+           classSize[6] += 1
+   for i in range(0,7):
+      print("Grade "+str(i)+":  " +str(classSize[i]))
+
 def query(cmd, f_in):
     if cmd[0] == 'S:':
       if len(cmd) == 2:
@@ -78,22 +119,29 @@ def query(cmd, f_in):
       if len(cmd) == 2:
          grade(cmd[1], f_in, 'N')
       elif len(cmd) == 3:
-         grade(cmd[1], f_in, cmd[2])
+        grade(cmd[1], f_in, cmd[2])
     elif cmd[0] == 'A:':
-      #average()
-      print(2)
-    elif cmd[0] == 'I:':
-      #info()
-      print(3)
-    elif cmd[0] == 'Q:':
+      average(cmd[1], f_in)
+    elif cmd[0] == 'I':
+      info(f_in)
+    elif cmd[0] == 'Q':
        return False
-    else:
-       exit(1)
     return True
 
 def main():
     print("Welcome to school search.")
     while True:
+       if path.exists("students.txt")==False:
+           print("File students.txt not found")
+           exit(1)
+       f_in = open('students.txt', 'r')
+       for line in f_in.readlines():
+          line = line.split(",")
+          if len(line) != 8:
+            print("Wrong format for students.txt")
+            f_in.close()
+            exit(1)
+       f_in.close()
        f_in = open('students.txt', 'r')
        command = input("Enter a search command: ")
        cmd = command.split(" ") 
